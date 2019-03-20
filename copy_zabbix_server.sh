edd472715b4e79661a908df52e26d32e363ad96f
Vill <zzzz@gmail.com>
2019-03-20 10:23:23 +0300

    added copy_zabbix_server in /mnt

--- copy_zabbix_server.sh
+++ copy_zabbix_server.sh
@@ -5,9 +5,9 @@
 ROPT="-v -az --delete"
  
 #Перенос директорий с настройками Zabbix Appache 
-rsync $ROPT /etc/zabbix/ /home/zabbixadmin/backup/zabbix_server/etc_zabbix
-rsync $ROPT /usr/share/zabbix/ /home/zabbixadmin/backup/zabbix_server/usr_share_zabbix
-rsync $ROPT /etc/httpd/ /home/zabbixadmin/backup/zabbix_server/etc_httpd
+rsync $ROPT /etc/zabbix/ /mnt/backup_zabbix/etc_zabbix
+rsync $ROPT /usr/share/zabbix/ /mnt/backup_zabbix/usr_share_zabbix
+rsync $ROPT /etc/httpd/ /mnt/backup_zabbix//etc_httpd
 #rsync $ROPT /etc/letsencrypt/ /backup/zabbix_server/etc_letsencrypt
 
 #Остановка сервиса Zabbix
@@ -15,10 +15,10 @@ systemctl stop zabbix-server
 sleep 3
 
 #Дамп БД
-mysqldump -uzabbix -p31832312 zabbix > /home/zabbixadmin/backup/zabbix_server/`date +%d-%m-%Y`.zabbix.sql
+mysqldump -uzabbix -p31832312 zabbix > /mnt/backup_zabbix/`date +%d-%m-%Y`.zabbix.sql
 
 #Запуск сервиса Zabbix
 service zabbix-server start
 
 #Создание архива 
-tar -czvf /home/zabbixadmin/backup/`date +%d-%m-%Y`.zabbix_server.tar.gz  /home/zabbixadmin/backup/zabbix_server
+tar -czvf /mnt/backup_zabbix/`date +%d-%m-%Y`.backup_zabbix.tar.gz  /mnt/backup_zabbix/
