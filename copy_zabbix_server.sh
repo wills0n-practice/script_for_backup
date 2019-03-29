0e2670d36233672e3b64295cf1a347346d221da6
Vill <zzzz@gmail.com>
2019-03-29 16:03:31 +0300

    added find, delete mysqldump

--- copy_zabbix_server.sh
+++ copy_zabbix_server.sh
@@ -5,9 +5,9 @@
 ROPT="-v -az --delete"
  
 #Перенос директорий с настройками Zabbix Appache 
-rsync $ROPT /etc/zabbix/ /mnt/backup_zabbix/etc_zabbix
-rsync $ROPT /usr/share/zabbix/ /mnt/backup_zabbix/usr_share_zabbix
-rsync $ROPT /etc/httpd/ /mnt/backup_zabbix//etc_httpd
+rsync $ROPT /etc/zabbix/ /mnt/backup_zabbix/zabbix_server/etc_zabbix
+rsync $ROPT /usr/share/zabbix/ /mnt/backup_zabbix/zabbix_server/usr_share_zabbix
+rsync $ROPT /etc/httpd/ /mnt/backup_zabbix/zabbix_server/etc_httpd
 #rsync $ROPT /etc/letsencrypt/ /backup/zabbix_server/etc_letsencrypt
 
 #Остановка сервиса Zabbix
@@ -15,10 +15,13 @@ systemctl stop zabbix-server
 sleep 3
 
 #Дамп БД
-mysqldump -uzabbix -p31832312 zabbix > /mnt/backup_zabbix/`date +%d-%m-%Y`.zabbix.sql
+mysqldump -uzabbix -p31832312 zabbix > /mnt/backup_zabbix/zabbix_server/`date +%d-%m-%Y`.zabbix.sql
 
 #Запуск сервиса Zabbix
 service zabbix-server start
 
 #Создание архива 
-tar -czvf /mnt/backup_zabbix/`date +%d-%m-%Y`.backup_zabbix.tar.gz  /mnt/backup_zabbix/
+tar -czvf /mnt/backup_zabbix/zabbix_server/`date +%d-%m-%Y`.backup_zabbix.tar.gz  /mnt/backup_zabbix/
+
+#Удаление дампа бд 3-ёх дневной давности
+find /mnt/backup_zabbix/zabbix_server/ -name "*.sql" -mtime +2 -exec rm -f {} \;
